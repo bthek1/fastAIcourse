572a56abf516b29f629a2584d79c2a6c03b7d9f7
main <bthekkel1@gmail.com>
2024-01-05 17:44:33 +1000

    "b'M  fastAIcourse/__init__.py\nM  fastAIcourse/datasets.py\nM  nbs/150_datasets.ipynb\nM  settings.ini\n'"

--- fastAIcourse/datasets.py
+++ fastAIcourse/datasets.py
@@ -3,20 +3,20 @@
 # %% auto 0
 __all__ = ['inplace', 'collate_dict', 'show_image', 'subplots', 'get_grid', 'show_images', 'DataLoaders']
 
-# %% ../nbs/150_datasets.ipynb 23
+# %% ../nbs/150_datasets.ipynb 24
 def inplace(f):
     def _f(b):
         f(b)
         return b
     return _f
 
-# %% ../nbs/150_datasets.ipynb 33
+# %% ../nbs/150_datasets.ipynb 34
 def collate_dict(ds):
     get = itemgetter(*ds.features)
     def _f(b): return get(default_collate(b))
     return _f
 
-# %% ../nbs/150_datasets.ipynb 37
+# %% ../nbs/150_datasets.ipynb 38
 @fc.delegates(plt.Axes.imshow)
 def show_image(im, ax=None, figsize=None, title=None, noframe=True, **kwargs):
     "Show a PIL or PyTorch image on `ax`."
@@ -33,7 +33,7 @@ def show_image(im, ax=None, figsize=None, title=None, noframe=True, **kwargs):
     if noframe: ax.axis('off')
     return ax
 
-# %% ../nbs/150_datasets.ipynb 41
+# %% ../nbs/150_datasets.ipynb 42
 @fc.delegates(plt.subplots, keep=True)
 def subplots(
     nrows:int=1, # Number of rows in returned axes grid
@@ -50,7 +50,7 @@ def subplots(
     if nrows*ncols==1: ax = np.array([ax])
     return fig,ax
 
-# %% ../nbs/150_datasets.ipynb 45
+# %% ../nbs/150_datasets.ipynb 46
 @fc.delegates(subplots)
 def get_grid(
     n:int, # Number of axes
@@ -72,7 +72,7 @@ def get_grid(
     if title is not None: fig.suptitle(title, weight=weight, size=size)
     return fig,axs
 
-# %% ../nbs/150_datasets.ipynb 47
+# %% ../nbs/150_datasets.ipynb 48
 @fc.delegates(subplots)
 def show_images(ims:list, # Images to show
                 nrows:int|None=None, # Number of rows in grid
@@ -83,7 +83,7 @@ def show_images(ims:list, # Images to show
     axs = get_grid(len(ims), nrows, ncols, **kwargs)[1].flat
     for im,t,ax in zip_longest(ims, titles or [], axs): show_image(im, ax=ax, title=t)
 
-# %% ../nbs/150_datasets.ipynb 51
+# %% ../nbs/150_datasets.ipynb 52
 class DataLoaders:
     def __init__(self, *dls): self.train,self.valid = dls[:2]
 
