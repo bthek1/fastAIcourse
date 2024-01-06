3b5fadb1c02678826e2a21b0fcd8f0fb903205e7
main <bthekkel1@gmail.com>
2024-01-06 19:32:21 +1000

    "b'M  fastAIcourse/__init__.py\nM  fastAIcourse/_modidx.py\nM  fastAIcourse/datasets.py\nM  nbs/150_datasets.ipynb\nM  nbs/170_convolutions.ipynb\nA  nbs/images/att_00028.png\nA  nbs/images/att_00029.png\nA  nbs/images/att_00030.png\nA  nbs/images/att_00032.png\nA  nbs/images/att_00033.png\nA  nbs/images/att_00034.png\nA  nbs/images/att_00035.png\nA  nbs/images/att_00036.png\nA  nbs/images/att_00042.png\nA  nbs/images/att_00068.png\nA  nbs/images/att_00069.png\nA  nbs/images/chapter9_baseconv.svg\nA  nbs/images/chapter9_bottleneck.png\nA  nbs/images/chapter9_cat_conv.png\nA  nbs/images/chapter9_conv_basic.png\nA  nbs/images/chapter9_conv_pad.png\nA  nbs/images/chapter9_conv_rgb.png\nA  nbs/images/chapter9_conv_stride.png\nA  nbs/images/chapter9_loss_landscape.png\nA  nbs/images/chapter9_nopadconv.svg\nA  nbs/images/chapter9_padconv.svg\nA  nbs/images/chapter9_rgb_conv_stack.svg\nA  nbs/images/chapter9_rgbconv.svg\nA  nbs/images/chapter9_skip_connection.png\nA  nbs/images/chapter9_strideconv.svg\nA  nbs/images/grizzly.jpg\nA  nbs/images/norms.png\nM  settings.ini\n'"

--- fastAIcourse/datasets.py
+++ fastAIcourse/datasets.py
@@ -12,7 +12,8 @@ from torch.utils.data import default_collate
 from .training import *
 
 # %% auto 0
-__all__ = ['collate_fn', 'transforms', 'inplace', 'transformi', 'D', 'collate_dict', 'show_image', 'subplots']
+__all__ = ['collate_fn', 'transforms', 'inplace', 'transformi', 'D', 'collate_dict', 'show_image', 'subplots', 'show_images',
+           'DataLoaders']
 
 # %% ../nbs/150_datasets.ipynb 4
 import logging,pickle,gzip,os,time,shutil,torch,matplotlib as mpl
@@ -96,3 +97,23 @@ def subplots(
 
 # %% ../nbs/150_datasets.ipynb 43
 from nbdev.showdoc import show_doc
+
+# %% ../nbs/150_datasets.ipynb 49
+@fc.delegates(subplots)
+def show_images(ims:list, # Images to show
+                nrows:int|None=None, # Number of rows in grid
+                ncols:int|None=None, # Number of columns in grid (auto-calculated if None)
+                titles:list|None=None, # Optional list of titles for each image
+                **kwargs):
+    "Show all images `ims` as subplots with `rows` using `titles`"
+    axs = get_grid(len(ims), nrows, ncols, **kwargs)[1].flat
+    for im,t,ax in zip_longest(ims, titles or [], axs): show_image(im, ax=ax, title=t)
+
+# %% ../nbs/150_datasets.ipynb 53
+class DataLoaders:
+    def __init__(self, *dls): self.train,self.valid = dls[:2]
+
+    @classmethod
+    def from_dd(cls, dd, batch_size, as_tuple=True, **kwargs):
+        f = collate_dict(dd['train'])
+        return cls(*get_dls(*dd.values(), bs=batch_size, collate_fn=f, **kwargs))
