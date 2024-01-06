fe1abfb7ec58da86cbe79ebadea7055a183484b6
main <bthekkel1@gmail.com>
2024-01-06 19:27:59 +1000

    "b'M  fastAIcourse/__init__.py\nM  fastAIcourse/_modidx.py\nM  fastAIcourse/conv.py\nM  fastAIcourse/datasets.py\nA  fastAIcourse/learner.py\nM  nbs/150_datasets.ipynb\nM  nbs/170_convolutions.ipynb\nA  nbs/180_autoencoder.ipynb\nA  nbs/190_learner.ipynb\nM  settings.ini\n'"

--- fastAIcourse/conv.py
+++ fastAIcourse/conv.py
@@ -3,7 +3,7 @@
 # %% auto 0
 __all__ = ['def_device', 'conv', 'to_device', 'collate_device']
 
-# %% ../nbs/170_convolutions.ipynb 2
+# %% ../nbs/170_convolutions.ipynb 3
 import torch
 from torch import nn
 
@@ -13,13 +13,13 @@ from typing import Mapping
 from .training import *
 from .datasets import *
 
-# %% ../nbs/170_convolutions.ipynb 69
+# %% ../nbs/170_convolutions.ipynb 70
 def conv(ni, nf, ks=3, stride=2, act=True):
     res = nn.Conv2d(ni, nf, stride=stride, kernel_size=ks, padding=ks//2)
     if act: res = nn.Sequential(res, nn.ReLU())
     return res
 
-# %% ../nbs/170_convolutions.ipynb 74
+# %% ../nbs/170_convolutions.ipynb 75
 def_device = 'mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def to_device(x, device=def_device):
