383e11c776fd3bb52c9ac68a9d62153396c6d690
main <bthekkel1@gmail.com>
2024-01-09 19:26:58 +1000

    "b'M  fastAIcourse/__init__.py\nM  fastAIcourse/_modidx.py\nM  fastAIcourse/activations.py\nM  nbs/190_learner.ipynb\nM  nbs/200_activations.ipynb\nM  settings.ini\n'"

--- fastAIcourse/activations.py
+++ fastAIcourse/activations.py
@@ -6,11 +6,30 @@ import random,math,torch,numpy as np,matplotlib.pyplot as plt
 import fastcore.all as fc
 from functools import partial
 
-from miniai.datasets import *
-from miniai.learner import *
+from .datasets import *
+from .learner import *
 
 # %% auto 0
-__all__ = ['set_seed', 'Hook', 'Hooks', 'HooksCallback', 'append_stats', 'get_hist', 'get_min', 'ActivationStats']
+__all__ = ['set_seed', 'conv', 'cnn_layers', 'fit', 'SequentialModel', 'append_stats', 'Hook', 'DummyCtxMgr', 'DummyList',
+           'Hooks', 'HooksCallback', 'get_hist', 'get_min', 'ActivationStats']
+
+# %% ../nbs/200_activations.ipynb 3
+import torch.nn.functional as F,matplotlib as mpl
+from pathlib import Path
+from operator import attrgetter,itemgetter
+from contextlib import contextmanager
+
+from torch import tensor,nn,optim
+import torchvision.transforms.functional as TF
+from datasets import load_dataset
+
+from fastcore.test import test_close
+
+torch.set_printoptions(precision=2, linewidth=140, sci_mode=False)
+mpl.rcParams['figure.constrained_layout.use'] = True
+
+import logging
+logging.disable(logging.WARNING)
 
 # %% ../nbs/200_activations.ipynb 4
 def set_seed(seed, deterministic=False):
@@ -19,13 +38,80 @@ def set_seed(seed, deterministic=False):
     random.seed(seed)
     np.random.seed(seed)
 
-# %% ../nbs/200_activations.ipynb 30
+# %% ../nbs/200_activations.ipynb 7
+def conv(ni, nf, ks=3, act=True):
+    res = nn.Conv2d(ni, nf, stride=2, kernel_size=ks, padding=ks//2)
+    if act: res = nn.Sequential(res, nn.ReLU())
+    return res
+
+def cnn_layers():
+    return [
+        conv(1 ,8, ks=5),        #14x14
+        conv(8 ,16),             #7x7
+        conv(16,32),             #4x4
+        conv(32,64),             #2x2
+        conv(64,10, act=False),  #1x1
+        nn.Flatten()]
+
+# %% ../nbs/200_activations.ipynb 9
+from torcheval.metrics import MulticlassAccuracy
+
+# %% ../nbs/200_activations.ipynb 11
+def fit(model, epochs=1, xtra_cbs=None):
+    learn = Learner(model, dls, loss_func=F.cross_entropy, lr=0.6, cbs=cbs+fc.L(xtra_cbs))
+    learn.fit(epochs)
+    return learn
+
+# %% ../nbs/200_activations.ipynb 15
+class SequentialModel(nn.Module):
+    def __init__(self, *layers):
+        super().__init__()
+        self.layers = nn.ModuleList(layers)
+        self.act_means = [[] for _ in layers]
+        self.act_stds  = [[] for _ in layers]
+        
+    def __call__(self, x):
+        for i,l in enumerate(self.layers):
+            x = l(x)
+            self.act_means[i].append(to_cpu(x).mean())
+            self.act_stds [i].append(to_cpu(x).std ())
+        return x
+    
+    def __iter__(self): return iter(self.layers)
+
+# %% ../nbs/200_activations.ipynb 25
+def append_stats(i, mod, inp, outp):
+    act_means[i].append(to_cpu(outp).mean())
+    act_stds [i].append(to_cpu(outp).std())
+
+# %% ../nbs/200_activations.ipynb 31
 class Hook():
     def __init__(self, m, f): self.hook = m.register_forward_hook(partial(f, self))
     def remove(self): self.hook.remove()
     def __del__(self): self.remove()
 
-# %% ../nbs/200_activations.ipynb 42
+# %% ../nbs/200_activations.ipynb 32
+def append_stats(hook, mod, inp, outp):
+    if not hasattr(hook,'stats'): hook.stats = ([],[])
+    acts = to_cpu(outp)
+    hook.stats[0].append(acts.mean())
+    hook.stats[1].append(acts.std())
+
+# %% ../nbs/200_activations.ipynb 38
+class DummyCtxMgr:
+    def __enter__(self, *args):
+        print("let's go!")
+        return self
+    def __exit__ (self, *args): print("all done!")
+    def hello(self): print("hello.")
+
+# %% ../nbs/200_activations.ipynb 40
+class DummyList(list):
+    def __delitem__(self, i):
+        print(f"Say bye to item {i}")
+        super().__delitem__(i)
+
+# %% ../nbs/200_activations.ipynb 43
 class Hooks(list):
     def __init__(self, ms, f): super().__init__([Hook(m, f) for m in ms])
     def __enter__(self, *args): return self
@@ -37,7 +123,7 @@ class Hooks(list):
     def remove(self):
         for h in self: h.remove()
 
-# %% ../nbs/200_activations.ipynb 46
+# %% ../nbs/200_activations.ipynb 47
 class HooksCallback(Callback):
     def __init__(self, hookfunc, mod_filter=fc.noop, on_train=True, on_valid=False, mods=None):
         fc.store_attr()
@@ -55,7 +141,7 @@ class HooksCallback(Callback):
     def __iter__(self): return iter(self.hooks)
     def __len__(self): return len(self.hooks)
 
-# %% ../nbs/200_activations.ipynb 51
+# %% ../nbs/200_activations.ipynb 52
 def append_stats(hook, mod, inp, outp):
     if not hasattr(hook,'stats'): hook.stats = ([],[],[])
     acts = to_cpu(outp)
@@ -63,16 +149,16 @@ def append_stats(hook, mod, inp, outp):
     hook.stats[1].append(acts.std())
     hook.stats[2].append(acts.abs().histc(40,0,10))
 
-# %% ../nbs/200_activations.ipynb 53
+# %% ../nbs/200_activations.ipynb 54
 # Thanks to @ste for initial version of histgram plotting code
 def get_hist(h): return torch.stack(h.stats[2]).t().float().log1p()
 
-# %% ../nbs/200_activations.ipynb 55
+# %% ../nbs/200_activations.ipynb 56
 def get_min(h):
     h1 = torch.stack(h.stats[2]).t().float()
     return h1[0]/h1.sum(0)
 
-# %% ../nbs/200_activations.ipynb 58
+# %% ../nbs/200_activations.ipynb 59
 class ActivationStats(HooksCallback):
     def __init__(self, mod_filter=fc.noop): super().__init__(append_stats, mod_filter)
 
